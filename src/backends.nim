4041b5bcc19692db273c566ac78cc46e64f940f2
Faisal Alghamdi <punchcake@skiff.com>
2023-09-12 11:29:08 +0300

    idk

---
+++ src/backends.nim
@@ -0,0 +1,28 @@
+import std/[
+    strformat,
+    strbasics,
+    os
+]
+
+
+proc addFolderToPath*(folderName: string) =
+    if folderName == "":
+        raise newException(Exception, "Input was empty")
+
+    var pathToEnvFile = ""
+    try:
+        var pathToEnvFile = fmt"{os.getHomeDir()}.bashrc"
+    except:
+        var pathToEnvFile = fmt"{os.getHomeDir()}.zshrc"
+
+    let file = open(pathToEnvFile, fmAppend)
+
+    # looks weird but i need to copy the string so i can pass it into strip.
+    var folderName = folderName
+
+    # remove whitespaces
+    strip(folderName)
+
+    file.writeLine(fmt"export PATH={folderName}:$PATH")
+
+    defer: file.close()
